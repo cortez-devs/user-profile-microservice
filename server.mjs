4065f9480e261e56e6e0f6b86a8c08ddc291c360
cortez-devs <cortezcr@oregonstate.edu>
2026-06-05 20:31:17 -0500

    Add CORS to allow api calls from frontend

--- server.mjs
+++ server.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 import profilesRouter from './routes/profiles.mjs';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/profiles", profilesRouter);
